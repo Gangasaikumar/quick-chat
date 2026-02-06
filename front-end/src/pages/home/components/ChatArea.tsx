b3aa7ba63ff13c967ca0f614f1b601d55c45362e
Gangasai kumar <gangasaikumar55555@gmail.com>
2026-02-06 13:21:52 +0530

    changeded bgs and some colors

--- front-end/src/pages/home/components/ChatArea.tsx
+++ front-end/src/pages/home/components/ChatArea.tsx
@@ -266,7 +266,7 @@ const ChatArea = () => {
                       {isSender(message) && message.read && (
                         <i
                           className="fa fa-check-circle"
-                          style={{ color: "#e74c3c" }}
+                          style={{ color: "var(--primary-color)" }}
                         />
                       )}
                     </div>
@@ -281,20 +281,15 @@ const ChatArea = () => {
               </div>
             </div>
             {showEmojiPicker && (
-              <EmojiPicker
-                onEmojiClick={(e) => {
-                  setMessage((prev) => prev + e.emoji);
-                }}
-                style={{
-                  width: "300px",
-                  height: "400px",
-                  position: "absolute",
-                  bottom: "110px",
-                  right: "110px",
-                  zIndex: 1000,
-                  filter: "drop-shadow(0 0 10px #ddd)",
-                }}
-              />
+              <div className="emoji-picker">
+                <EmojiPicker
+                  onEmojiClick={(e) => {
+                    setMessage((prev) => prev + e.emoji);
+                  }}
+                  width={300}
+                  height={400}
+                />
+              </div>
             )}
             <div className="send-message-div">
               <label htmlFor="file">
